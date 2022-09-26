c645d5a08b224cd3fa10fe4bbfe1f9c40e883fed
NeemoF <neemaf@outlook.com>
2022-09-26 15:20:56 +0100

    ree

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+threeDArray = [
+    [
+        ["hello", "i", "love", "cheese"]
+        ["bye", "i","hate","mayonnaise"]
+    ]
+    [
+        ["James", "wrote", "t", "in", "the", "classroom"]
+        ["It", "was", "funny"]
+    ]
+]
